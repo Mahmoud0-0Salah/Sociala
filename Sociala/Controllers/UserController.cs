66c4d99bcbd64f132aefeb861ced30387b6a61e8
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2024-02-29 08:49:44 +0200

    add logininfo viewmodel

---
+++ Sociala/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Security.Cryptography;
+using Sociala.Data;
+using Sociala.Models;
+using Sociala.ViewModel;
+using EncryptServices;
+using EmailSendertServices;
+
+namespace Sociala.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
