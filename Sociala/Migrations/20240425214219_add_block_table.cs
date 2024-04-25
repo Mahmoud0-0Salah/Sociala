f36900f917efa54e183ee02e2d55bd0dd9c7b519
anwar <mo.anwar26102002@gmail.com>
2024-04-26 01:15:54 +0300

    make table block

---
+++ Sociala/Migrations/20240425214219_add_block_table.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sociala.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_block_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Block",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Blocking = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    Blocked = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Block", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Block_User_Blocking",
+                        column: x => x.Blocking,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Block_Blocking",
+                table: "Block",
+                column: "Blocking");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Block");
+        }
+    }
+}
