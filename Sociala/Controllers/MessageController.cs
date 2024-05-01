34ea7099e4db90bc7fdcd6d49f8ef09adcd5a94c
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2024-05-01 18:49:03 +0300

    Make chat real time

--- Sociala/Controllers/MessageController.cs
+++ Sociala/Controllers/MessageController.cs
@@ -1,7 +1,10 @@
 ﻿using AuthorizationService;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Sociala.Data;
+using Sociala.Hubs;
 using Sociala.Models;
+using System.Text.Json;
 
 namespace Sociala.Controllers
 {
@@ -10,14 +13,15 @@ namespace Sociala.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly AppData _data;
         private readonly IAuthorization authorization;
-
-        public MessageController(ILogger<HomeController> logger, AppData data, IAuthorization authorization)
+        private readonly IHubContext<NotificationsHub> _hubContext;
+        public MessageController(ILogger<HomeController> logger, AppData data, IAuthorization authorization, IHubContext<NotificationsHub> hubContext)
         {
             _logger = logger;
             _data = data;
             this.authorization = authorization;
+            _hubContext = hubContext;
         }
-        public IActionResult CreateMessage(string id, string content)
+        public async Task<IActionResult> CreateMessage(string id, string content)
         {
             string UserId = authorization.GetId();
             Message message = new Message();
@@ -26,6 +30,20 @@ namespace Sociala.Controllers
             message.ResverId = id;
             _data.Message.Add(message);
             _data.SaveChanges();
+
+
+            string jsonData = JsonSerializer.Serialize(message);
+            var _userConnections = NotificationsHub.GetUsersConnections();
+            if (!string.IsNullOrEmpty(id) && _userConnections.TryGetValue(id, out var connectionIds))
+            {
+                foreach (var connectionId in connectionIds)
+                {
+                    await _hubContext.Clients.Client(connectionId).SendAsync("Message", jsonData);
+                }
+            }
+
+
+
             var Message = _data.Message.Where(m=>(m.SenderId.Equals(UserId) && m.ResverId.Equals(id)|| m.SenderId.Equals(id) && m.ResverId.Equals(UserId)));
             ViewBag.UserId=UserId;
             ViewBag.Sendder = _data.User.Where(u=>u.Id.Equals(id)).SingleOrDefault();
---
+++ Sociala/wwwroot/js/message.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/notifications").build();
+
+connection.on("Message", function (message) {
+
+    // No need to parse the message since it's already in JSON format
+    var messageData = JSON.parse(message);
+
+    // Create the message container
+    var messageContainer = document.createElement('div');
+    messageContainer.classList.add('message');
+
+    // Create the message content element
+    var messageContent = document.createElement('div');
+    messageContent.classList.add('message-content', 'font-xssss', 'lh-24', 'fw-500');
+    messageContent.textContent = messageData.Content;
+
+    // Append message content to message container
+    messageContainer.appendChild(messageContent);
+
+    // Append message container to messages section at the end
+    var messagesSection = document.querySelector('.messages');
+    messagesSection.insertBefore(messageContainer, messagesSection.firstChild);
+
+    // Scroll to bottom of messages
+    messagesSection.scrollTop = messagesSection.scrollHeight;
+
+});
+
+connection.start().then(function () {
+    console.log("Connection Started");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+function SendMessage(content, receiverId) {
+    connection.invoke("CreateMessage", receiverId, content).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
